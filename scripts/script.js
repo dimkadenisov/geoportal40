84877a2fd8f222965bf32bc81a273c13d4e0743b
dimkadenisov <2denisov@bk.ru>
2020-09-13 22:45:37 +0000

    Deploying to gh-pages from  @ 276b758e595e75ff7ae0b2a1459cbf6c8729ef95 🚀

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,18 @@ const linksSwiper = document.querySelector('.hero-swiper__inner') && new Swiper(
     prevEl: '.swiper-button-prev'
   }
 });
+"use strict";
+
+document.body.addEventListener('click', e => {
+  const switchInput = e.target.closest('[data-id="order-data-form-switch"]');
+  if (!switchInput) return;
+  console.log('event');
+
+  if (switchInput.checked) {
+    document.querySelector('[data-id="ul"]').classList.remove('visually-hidden');
+    document.querySelector('[data-id="phis"]').classList.add('visually-hidden');
+  } else {
+    document.querySelector('[data-id="ul"]').classList.add('visually-hidden');
+    document.querySelector('[data-id="phis"]').classList.remove('visually-hidden');
+  }
+});
