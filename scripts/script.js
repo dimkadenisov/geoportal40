2c9fcf59fd72399a155c37a1fb1d28ca44e407e7
dimkadenisov <2denisov@bk.ru>
2020-09-17 14:34:32 +0000

    Deploying to gh-pages from  @ c82f91cdc92688e807dc606a12dfdd9ca6e53b31 🚀

--- scripts/script.js
+++ scripts/script.js
@@ -29,6 +29,20 @@ const linksSwiper = document.querySelector('.hero-swiper__inner') && new Swiper(
 "use strict";
 
 document.body.addEventListener('click', e => {
+  const orderDataButton = e.target.closest('.order-data-button');
+  if (!orderDataButton) return;
+  orderDataButton.classList.toggle('order-data-button_opened');
+});
+document.body.addEventListener('click', e => {
+  const orderDataButton = document.querySelector('.order-data-button');
+
+  if (orderDataButton && orderDataButton.classList.contains('order-data-button_opened') && !e.target.closest('.order-data-button')) {
+    orderDataButton.classList.remove('order-data-button_opened');
+  }
+});
+"use strict";
+
+document.body.addEventListener('click', e => {
   const switchInput = e.target.closest('[data-id="order-data-form-switch"]');
   if (!switchInput) return;
 
