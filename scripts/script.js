3a3758a3047d7002b3fc7334f669098f59af8d1b
dimkadenisov <2denisov@bk.ru>
2020-09-22 19:28:05 +0000

    Deploying to gh-pages from  @ 4430b8d55c9a118642effd4e6de33dae0ce86769 🚀

--- scripts/script.js
+++ scripts/script.js
@@ -28,6 +28,33 @@ document.body.addEventListener('click', e => {
 });
 "use strict";
 
+const inputs = document.querySelectorAll('.inputfile');
+
+for (let i = 0; i < inputs.length; i++) {
+  const input = inputs[i];
+  const label = input.nextElementSibling;
+  const labelVal = label.innerHTML;
+  input.addEventListener('change', function (e) {
+    const fileName = this.files && this.files.length > 1 ? 'Выбрано ' + this.files.length + ' файлов' : e.target.value.split('\\').pop();
+    if (fileName) label.innerHTML = fileName;else label.innerHTML = labelVal;
+  });
+}
+
+document.body.addEventListener('change', e => {
+  const fileInput = e.target.closest('.file-selector__input');
+  if (!fileInput) return;
+  const fileName = e.target.value.split('\\').pop();
+  fileInput.parentNode.parentNode.querySelector('.file-selector__filename').innerText = fileName;
+});
+document.body.addEventListener('submit', e => {
+  const form = e.target.closest('form');
+  if (!form) return;
+  e.preventDefault();
+  document.querySelector('#biba');
+  console.log(e);
+});
+"use strict";
+
 const linksSwiper = document.querySelector('.hero-swiper__inner') && new Swiper('.hero-swiper__inner', {
   slidesPerView: 1,
   spaceBetween: 0,
